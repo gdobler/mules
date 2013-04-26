f39d750fc75623947186f0136da082d5ee84c57e
Greg Dobler <greg.dobler@gmail.com>
2013-04-26 14:25:53 -0700

    recursion cleaned up for the process of breaking up a cell into subcells

--- py/ml_defang.py
+++ py/ml_defang.py
@@ -7,24 +7,32 @@ glones  = counters.glones
 
 def ml_defang(ximg, yimg, cells, stars, cellnum):
 
-# -------- initialize the deflection angle and run lowest level cell
-    if cellnum==None:
-        defang0 = ml_defang(ximg, yimg, cells, stars, 0)
-        defang1 = ml_defang(ximg, yimg, cells, stars, 1)
-        defang2 = ml_defang(ximg, yimg, cells, stars, 2)
-        defang3 = ml_defang(ximg, yimg, cells, stars, 3)
+    # -------- break up cell
+    def breakup_cell(cellnum):
+        basecell = 0 if cellnum==None else 4*(cellnum+1)
+        defang0  = ml_defang(ximg,yimg,cells,stars,basecell+0)
+        defang1  = ml_defang(ximg,yimg,cells,stars,basecell+1)
+        defang2  = ml_defang(ximg,yimg,cells,stars,basecell+2)
+        defang3  = ml_defang(ximg,yimg,cells,stars,basecell+3)
+        defang   = defang0[0] + defang1[0] + defang2[0] + defang3[0], \
+                   defang0[1] + defang1[1] + defang2[1] + defang3[1]
 
-#        print "ML_DEFANG: MUST SORT CLIST!!!"
-
-        return defang0[0] + defang1[0] + defang2[0] + defang3[0], \
-            defang0[1] + defang1[1] + defang2[1] + defang3[1]
+        return defang
 
 
 
-# -------- utilities
+    # -------- begin running lowest level cell
+    if cellnum==None: return breakup_cell(cellnum)
+
+
+
+    # -------- utilities
     delx   = ximg - cells[cellnum].xcell
     dely   = yimg - cells[cellnum].ycell
     rcell  = np.sqrt(delx*delx + dely*dely)
+
+    if rcell<1.0e-5: return breakup_cell(cellnum)
+
     tcell  = np.arctan2(dely,delx)
     mcell  = cells[cellnum].mcell
     cmom   = cells[cellnum].cmom
@@ -38,7 +46,7 @@ def ml_defang(ximg, yimg, cells, stars, cellnum):
 
 
 
-# -------- calculate the deflection angle up to order 20
+    # -------- calculate the deflection angle up to order 20
     nmm      = cmom.size
     mm       = mmrange[0:nmm] + 1.0
     oormp1   = rcell**(-mm-1.0)
@@ -65,27 +73,22 @@ def ml_defang(ximg, yimg, cells, stars, cellnum):
             mi2       = mi*mi
             delxi     = ximg - stars.xstar[stind]
             delyi     = yimg - stars.ystar[stind]
-            ri2       = delxi*delxi + delyi*delyi
-            alphax    = np.dot(mi2*(delxi/ri2),glones[:stind.size])
-            alphay    = np.dot(mi2*(delyi/ri2),glones[:stind.size])
+            oori2     = 1.0/(delxi*delxi + delyi*delyi)
+            alphax    = np.dot(mi2*delxi*oori2,glones[:stind.size])
+            alphay    = np.dot(mi2*delyi*oori2,glones[:stind.size])
             defang    = alphax, alphay
 
             counters.clist.append(-cellnum)
 
         else:
-            defang0 = ml_defang(ximg,yimg,cells,stars,4*(cellnum+1))
-            defang1 = ml_defang(ximg,yimg,cells,stars,4*(cellnum+1)+1)
-            defang2 = ml_defang(ximg,yimg,cells,stars,4*(cellnum+1)+2)
-            defang3 = ml_defang(ximg,yimg,cells,stars,4*(cellnum+1)+3)
-            defang  = defang0[0] + defang1[0] + defang2[0] + defang3[0], \
-                defang0[1] + defang1[1] + defang2[1] + defang3[1]
+            return breakup_cell(cellnum)
     else:
         counters.cellcnt += 1
 
         talphar   = np.dot(alphar,glones[0:20]) + alphar0
         talphat   = np.dot(alphat,glones[0:20])
-        alphax    = delx/rcell*talphar - dely/rcell*talphat
-        alphay    = dely/rcell*talphar + delx/rcell*talphat
+        alphax    = (delx*talphar - dely*talphat)/rcell
+        alphay    = (dely*talphar + delx*talphat)/rcell
         defang    = alphax, alphay
 
         counters.clist.append(cellnum)
