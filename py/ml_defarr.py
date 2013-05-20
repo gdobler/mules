1ad73a22270d8c0506c78f83be28d6de99955005
Greg Dobler <greg.dobler@gmail.com>
2013-05-20 13:42:34 -0700

    added subgridding spline interpolatin

--- py/ml_defarr.py
+++ py/ml_defarr.py
@@ -10,8 +10,8 @@ def ml_defarr(xr, yr, nx, ny, cells, stars, multi=None, recur=None,
     # -------- defaults
     multi    = 4    if multi==None else multi
     recur    = True if recur==None else recur
-    bins     = 0    if bins==None  else bins
-    def_func = ml_defang if recur else ml_defang_iter
+    bins     = 1    if bins==None  else bins
+    def_func = ml_defang if recur  else ml_defang_iter
 
 
     # -------- utilities
@@ -85,129 +85,33 @@ def ml_defarr(xr, yr, nx, ny, cells, stars, multi=None, recur=None,
     else:
         defarr = run_rect(-314,xr,yr,nx,ny,cells,stars,verbose=True)
 
-    return defarr
 
-"""
-# -------- linearly interpolate in fixed cells
-    elif scheme==2:
+    # -------- apply subgrid interpolation if desired
+    if bins>1:
+        print("ML_DEFARR: applying subgrid spline interpolation " + 
+              "with bins = {0}".format(bins))
 
-        # -------- defaults and utilities
-        if bins==None: bins = 64
-        defur  = np.zeros(2)
-        deful  = np.zeros(2)
-        deflr  = np.zeros(2)
-        defll  = np.zeros(2)
-        bp1    = bins + 1                  #
-        xind   = np.arange(bp1*bp1) % bp1 #
-        yind   = np.arange(bp1*bp1) / bp1 # utilities to be used 
-        xfrac  = xind/float(bins)          # for the interpolation
-        yfrac  = yind/float(bins)          #
-        ncx    = (nx-1)/bins
-        ncy    = (ny-1)/bins
-        x0, x1 = xr
-        y0, y1 = yr
-        ddx    = (x1-x0)/float(nx)
-        ddy    = (y1-y0)/float(ny)
+        # -------- allocate memory
+        defarr_fine = np.zeros([nx*bins,ny*bins,2],dtype=np.float64)
 
-        print "ML_DEFARR: linearly interpolating 2D img plane with bins= ", \
-            bins
+        # -------- define coarse and fine grids
+        xcoar = np.linspace(xr[0],xr[1],nx,endpoint=False)
+        ycoar = np.linspace(yr[0],yr[1],ny,endpoint=False)
+        ximg  = np.linspace(xr[0],xr[1],nx*bins,endpoint=False)
+        yimg  = np.linspace(yr[0],yr[1],ny*bins,endpoint=False)
 
-        for icx in range(ncx):
-            print "ML_DEFARR:   icx = {0} out of {1}\r".format(icx+1,ncx), 
-            sys.stdout.flush()
+        # -------- create the two interpolations
+        xm, ym    = np.meshgrid(xcoar,ycoar,indexing='ij')
+        coarint_x = interpolate.RectBivariateSpline(xcoar,ycoar,defarr[:,:,0])
+        coarint_y = interpolate.RectBivariateSpline(xcoar,ycoar,defarr[:,:,1])
 
-            # -------- get four corners in x
-            iximgul = icx*bins
-            iximgll = iximgul
-            iximgur = iximgul+bins
-            iximglr = iximgll+bins
+        # -------- run the interpolation
+        print("ML_DEFARR:   interpolating alpha_x...")
+        defarr_fine[:,:,0] = coarint_x(ximg,yimg)
+        print("ML_DEFARR:   interpolating alpha_y...")
+        defarr_fine[:,:,1] = coarint_y(ximg,yimg)
 
-            ximgul = x0 + ddx*float(iximgul)
-            ximgur = x0 + ddx*float(iximgur)
-            ximgll = x0 + ddx*float(iximgll)
-            ximglr = x0 + ddx*float(iximglr)
-
-            for icy in range(ncy):
-
-                # -------- get four corners in y
-                iyimgll = icy*bins
-                iyimglr = icy*bins
-                iyimgul = iyimgll+bins
-                iyimgur = iyimglr+bins
-
-                yimgul = y0 + ddy*float(iyimgul)
-                yimgur = y0 + ddy*float(iyimgur)
-                yimgll = y0 + ddy*float(iyimgll)
-                yimglr = y0 + ddy*float(iyimglr)
-
-                # -------- calculate the deflection angles, only need
-                #          to do all the corners for special cases
-                defur[:] = ml_defang(ximgur, yimgur, cells, stars, None)
-
-                if icx==0:
-                    deful[:] = ml_defang(ximgul, yimgul, cells, stars, None)
-                else:
-                    deful[:] = defarr[iximgul,iyimgul,:]
-
-                if icy==0:
-                    deflr[:] = ml_defang(ximglr, yimglr, cells, stars, None)
-                else:
-                    deflr[:] = defarr[iximglr,iyimglr,:]
-
-                if (icx==0) & (icy==0):
-                    defll[:] = ml_defang(ximgll, yimgll, cells, stars, None)
-                else:
-                    defll[:] = defarr[iximgll,iyimgll,:]
-
-
-                # -------- bi-linearly interpolate from the four corners
-                defarr[xind+iximgll,yind+iyimgll,0] = defll[0]*(1.0-xfrac)* \
-                    (1.0-yfrac) + deflr[0]*xfrac*(1.0-yfrac)+ \
-                    deful[0]*(1.0-xfrac)*yfrac + defur[0]*xfrac*yfrac
-                defarr[xind+iximgll,yind+iyimgll,1] = defll[1]*(1.0-xfrac)* \
-                    (1.0-yfrac) + deflr[1]*xfrac*(1.0-yfrac)+ \
-                    deful[1]*(1.0-xfrac)*yfrac + defur[1]*xfrac*yfrac
-
-        print
-        return
-
-# -------- evaluate grid and then spline
-    elif scheme==3:
-
-        print "ML_DEFARR: spline interpolating 2D grid..."
-
-        # -------- defaults and utilities
-        if bins==None: bins = 64
-
-        cnx    = nx/bins
-        cny    = ny/bins
-        coarse = np.zeros([cnx,cny,2])
-        xcoar  = np.linspace(xr[0],xr[1],num=cnx,endpoint=False)
-        ycoar  = np.linspace(yr[0],yr[1],num=cny,endpoint=False)
-
-        for iximg in range(cnx):
-            print 'ML_DEFARR:   iximg = {0} out of {1}\r'.format(iximg+1,cnx), 
-            sys.stdout.flush()
-
-            for iyimg in range(cny):
-                coarse[iximg,iyimg,:] = ml_defang(xcoar[iximg],ycoar[iyimg], 
-                                                  cells,stars,None)
-        print
-
-
-        # -------- interpolate from the coarse grid onto the real grid
-        ximg = np.linspace(xr[0],xr[1],num=nx,endpoint=False)
-        yimg = np.linspace(yr[0],yr[1],num=ny,endpoint=False)
-
-        defxint = interpolate.RectBivariateSpline(xcoar,ycoar,coarse[:,:,0])
-        defyint = interpolate.RectBivariateSpline(xcoar,ycoar,coarse[:,:,1])
-
-        defarr[:,:,0] = defxint(ximg,yimg)
-        defarr[:,:,1] = defyint(ximg,yimg)
-
-        return
-
+        # -------- return the interpolated array
+        return defarr_fine
     else:
-        print "ML_DEFARR: ONLY SCHEMES #1, 2, and 3 ARE IMPLEMENTED!!!"
-        return
-"""
+        return defarr
